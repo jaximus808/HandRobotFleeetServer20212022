2a73c9e1d49d82b9b5dbc9e593a8b69f75a9061a
jaximus808 <jaximus808@gmail.com>
2021-09-15 00:12:32 -1000

    Basic python communicator works

--- Assets/Scripts/PythonCommunicator.cs
+++ Assets/Scripts/PythonCommunicator.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
+using System.Threading;
 
 public class PythonCommunicator : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public bool active = false; 
+
+    public int inPort = 8000; //port to receive data
+    public int outPort = 8001; //port to send data
+
+    private UdpClient client; 
+    private IPEndPoint remoteEndPoint; 
+    private Thread receiveThread;
+
+    public void SendData(string message)
     {
-        
+        try 
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            client.Send(data, data.Length, remoteEndPoint);
+        }
+        catch(Exception error)
+        {
+            Debug.Log(error.ToString());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        remoteEndPoint = new IPEndPoint(IPAddress.Any, outPort);
+
+        client = new UdpClient(inPort);
+
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true; 
+        receiveThread.Start();
+        Debug.Log("Listening");
+
+    }
+
+    private void ReceiveData()
+    {
+        while (true)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+                string text = Encoding.UTF8.GetString(data);
+                print(">> " + text);
+                ProcessInput(text);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+    private void ProcessInput(string input)
+    {
+        // PROCESS INPUT RECEIVED STRING HERE
+
+        if (!active) // First data arrived so tx started
+        {
+            active = true;
+        }
+    }
+
+    //Prevent crashes - close clients and threads properly!
+    void OnDisable()
+    {
+        if (receiveThread != null)
+            receiveThread.Abort();
+
+        client.Close();
     }
 }
--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -4,34 +4,8 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
-using System.Threading.Tasks;
 
 public class Server
 {
-    public int ip {get;private set;}
-    public int port {get; private set;}
-    public void Start(string _ip, int _port)
-    {
-        port = _port;
-        var endPoint = new IPEndPoint(IPAddress.Loopback, port);
-        
-        var socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Udp);
-        socket.Bind(endPoint);
-        socket.Listen(128);
-
-        _ = Task.Run( ()=> DoEcho(socket));
-
-    }
-
-    private async Task DoEcho(Socket socket)
-    {
-        do 
-        {
-            var clientSocket = await Task.Factory.FromAsync(
-                new Func<AsyncCallback, object, IAsyncResult>(socket.BeginAccept),
-                new Func<IAsyncResult, Socket>(socket.EndAccept),
-                null).ConfigureAwait(false);
-        }
-        while(true);
-    }
+   
 }
